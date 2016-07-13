0490dcab3053b5931d4f4ddfb1a7a958e1e9526b
caden <cadenkline9@gmail.com>
2016-07-12 15:40:59 -0900

    working

--- src/com/k_lindev/gui/Main.java
+++ src/com/k_lindev/gui/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("chat.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("setup.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 600, 350));
         primaryStage.show();
---
+++ src/com/k_linedev/networking/Server.java
@@ -0,0 +1,10 @@
+package com.k_linedev.networking;
+
+/**
+ * Created by caden on 7/12/16.
+ */
+public class Server {
+    public Server(){
+
+    }
+}
