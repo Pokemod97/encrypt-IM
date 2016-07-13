95b106a30ec89702e5501eef38a35d1a68855e6e
caden <cadenkline9@gmail.com>
2016-07-12 16:47:17 -0900

    working

--- src/com/k_linedev/networking/Server.java
+++ src/com/k_linedev/networking/Server.java
@@ -3,8 +3,15 @@ package com.k_linedev.networking;
 /**
  * Created by caden on 7/12/16.
  */
-public class Server {
+public class abstract Server {
+    private int port;
     public Server(){
-
+        port = 1234;
     }
+    public Server(int port){
+        port = 1234;
+        }
+    public abstract listen(){
+
+        }
 }
